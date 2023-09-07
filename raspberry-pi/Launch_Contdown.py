218b7d1eccf46f76fedd4c168153b386cd681ed0
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-09-07 10:45:34 -0400

    2 done

--- raspberry-pi/Launch_Contdown.py
+++ raspberry-pi/Launch_Contdown.py
@@ -1,12 +1,23 @@
 import board
 import time
+import digitalio
 Seconds = 10 # Starts at 10 seconds
+Gled = digitalio.DigitalInOut(board.GP0) # Sets the pin of the green LED
+Rled = digitalio.DigitalInOut(board.GP1) # Sets the pin of the red LED
+Gled.direction = digitalio.Direction.OUTPUT # Pin is using output
+Rled.direction = digitalio.Direction.OUTPUT # Pin is using output
 
 while True:
     if Seconds > 0: # When it is counting down
         print("T minus", Seconds) # Prints to serial monitor
-        time.sleep(1) # Pause 1 second
+        time.sleep(.5) # Pause 1/2 second
+        Gled.value = True
+        time.sleep(.5) # Pause 1/2 second
+        Gled.value = False
         Seconds = Seconds - 1 # Decreases the value of seconds by 1
     else: # Once it reaches 0
         print("LIFTOFF") # Prints to serial monitor
-        time.sleep(1) # Pause 1 second
+        time.sleep(.5) # Pause 1/2 second
+        Rled.value = True
+        time.sleep(.5) # Pause 1/2 second
+        Rled.value = False
