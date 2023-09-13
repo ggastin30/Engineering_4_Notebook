7a519b6b7e79d4a6b7a083107cdb8b640604a3e9
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-09-13 10:33:46 -0400

    4 Done

--- raspberry-pi/Launchpad_4.py
+++ raspberry-pi/Launchpad_4.py
@@ -1,11 +1,13 @@
 import board
 import time
 import digitalio
+import pwmio
+from adafruit_motor import servo
 
 Seconds = 10 # Starts at 10 seconds
 Bpress = 0 # Button presses
 
-Button = digitalio.DigitalInOut(board.GP16)
+Button = digitalio.DigitalInOut(board.GP16) 
 Rled = digitalio.DigitalInOut(board.GP0) # Sets the pin of the green LED
 Gled = digitalio.DigitalInOut(board.GP1) # Sets the pin of the red LED
 Button.direction = digitalio.Direction.INPUT # Pin is using input
@@ -13,11 +15,16 @@ Rled.direction = digitalio.Direction.OUTPUT # Pin is using output
 Gled.direction = digitalio.Direction.OUTPUT # Pin is using output
 Button.pull = digitalio.Pull.UP # Button has normal amount set at 3 volts
 
+pwm = pwmio.PWMOut(board.GP15, duty_cycle=2 ** 15, frequency=50) # create a PWMOut object on Pin A2.
+my_servo = servo.Servo(pwm, min_pulse=500, max_pulse=2500) # Define the servo
+
+
 while True:
     if Bpress < 1: # If it's not pressed
         if Button.value == False: # If pressed
             Bpress = 1 # Go to countdown
     else:
+        my_servo.angle = 0 # Servo angle goes to 0
         Gled.value = False # Turn off green from previous loop
         if Seconds > 0: # When it is counting down
             print("T minus", Seconds) # Prints to serial monitor
@@ -29,6 +36,6 @@ while True:
         else: # Once it reaches 0
             print("LIFTOFF") # Prints to serial monitor
             Gled.value = True # Turn on Green
-            if Button.value == False: # If pressed
-                Seconds = 10 # Reset values for new loop
-                Bpress = 0
+            my_servo.angle = 179 # 180 glitches the servo
+            Seconds = 10 # Reset values for new loop
+            Bpress = 0 # Resest button
