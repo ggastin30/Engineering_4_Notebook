31e5f3ad8fd240d9c2ca2a624cb4455894faa105
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-09-22 10:11:34 -0400

    G

--- raspberry-pi/Crash_Avoidance_3.py
+++ raspberry-pi/Crash_Avoidance_3.py
@@ -7,6 +7,7 @@ import adafruit_displayio_ssd1306
 import terminalio
 import displayio
 import digitalio
+import math
 
 displayio.release_displays()
 
@@ -18,15 +19,15 @@ i2c = busio.I2C(scl_pin, sda_pin) # Connects the sda and scl to i2c
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP3) # Connect OLED to i2c
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64) # Define OLED
 splash = displayio.Group() # create the display group
-title = "ANGULAR VELOCITY" # add title block to display group
-text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5) # the order of this command is (font, text, text color, and location)
-splash.append(text_area)    
-display.show(splash)
 
 Led = digitalio.DigitalInOut(board.GP0) # Set LED pin
 Led.direction = digitalio.Direction.OUTPUT # Pin is using output
 
 mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68) # Sets up the MPU
+title = "ANGULAR VELOCITY" # add title block to display group
+text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5) # the order of this command is (font, text, text color, and location)
+splash.append(text_area)   
+display.show(splash)
 
 while True:
    
@@ -34,13 +35,12 @@ while True:
     Yaccel = mpu.acceleration[1]
     Zaccel = mpu.acceleration[2]
 
-    Angular = mpu.gyro[0]
-
     #print(f"X acceleration:{Xaccel}") # Prints the X acceleration
     #print(f"Y acceleration:{Yaccel}")
     #print(f"Z acceleration:{Zaccel}")
-    print(f"Angular Velocity{Angular}")
     print(" ") # Create a space between the prints
+
+    text_area.text = f"ANGULAR VELOCITY: \n X:{round(mpu.gyro[0],3)} \n Y:{round(mpu.gyro[1],3)} \n Z:{round(mpu.gyro[2],3)}" 
 
     if abs(Zaccel) < 1: # If Z is 90 degrees from parallel
         Led.value = True # Turn LED on
