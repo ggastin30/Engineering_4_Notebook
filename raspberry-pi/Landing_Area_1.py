5255bfde485da562d026ee139b81c16d2f64b219
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-10-20 10:27:50 -0400

    LAnd 1 done?

--- raspberry-pi/Landing_Area_1.py
+++ raspberry-pi/Landing_Area_1.py
@@ -1,21 +1,39 @@
 import board
 
-
-
-
-def TriArea(x1,y1, x2,y2, x3,y3):
-    area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
-    return int(area)
-
+def TriArea(x1,y1,x2,y2,x3,y3):
+    try:
+        area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+        return area
+    except:
+        print("Invalid Coordinates")
+        area = 0
+        return area
 
 while True:
     
-    print("Point 1 Coordinates:")
-    x1 = input()
-    y1 = input()
-    print("Point 1 Coordinates:")
-    x2 = input()
-    y2 = input()
-    print("Point 1 Coordinates:")
-    x3 = input()
-    y3 = input()
+    print("Point 1 Coordinates (x,y):")
+    p1 = input()
+    x1, y1 = p1.split(",")
+    print(f"Point 1: ({p1})")
+
+    print("Point 2 Coordinates (x,y):")
+    p2 = input()
+    x2, y2 = p2.split(",")
+    print(f"Point 2: ({p2})")
+
+    print("Point 3 Coordinates (x,y):")
+    p3 = input()
+    x3, y3 = p3.split(",")
+    print(f"Point 3: ({p3})")
+
+    x1 = float(x1)
+    x2 = float(x2)
+    x3 = float(x3)
+    y1 = float(y1)
+    y2 = float(y2)
+    y3 = float(y3)
+    
+    area = TriArea(x1,y1,x2,y2,x3,y3)
+    print(" ")
+    print(f"AREA IS: {area}")
+    print(" ")
