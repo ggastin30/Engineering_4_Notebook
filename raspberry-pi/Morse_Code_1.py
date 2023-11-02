fc860020f4db9007c06e7b721b3b64afdba645ac
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-11-02 10:28:11 -0400

    Done w/ 1

--- raspberry-pi/Morse_Code_1.py
+++ raspberry-pi/Morse_Code_1.py
@@ -21,11 +21,11 @@ MORSE_CODE = { 'A':'.-', 'B':'-...',
 
 while True:
     print("Enter Morse Code Message, or enter -q to quit:")
-    Text = input()
-    if Text == "-q":
+    Text = input() #Get the input from the user
+    if Text == "-q": #If they type -q it quits 
         print("Quit")
-        break
+        break #end the code
     else: 
-        Up_Text = Text.upper()
-        for letter in Up_Text:
-            print(MORSE_CODE[f"{letter}"], end=" ")
+        Up_Text = Text.upper() #Make all text uppercase
+        for letter in Up_Text: #Go through each letter in the word
+            print(MORSE_CODE[f"{letter}"], end=" ") #Print each letter in morse
