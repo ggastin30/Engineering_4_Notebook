e0675ec3c55e3cd2797fb3511ef82b5b89145bf8
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-11-10 10:44:45 -0500

    nEEDS WORK

--- raspberry-pi/Data_1.py
+++ raspberry-pi/Data_1.py
@@ -1,3 +1,4 @@
+#type:ignore
 import board
 import time
 import adafruit_mpu6050
@@ -8,24 +9,31 @@ sda_pin = board.GP14 #Sets the sda pin
 scl_pin = board.GP15 #Sets the scl pin
 i2c = busio.I2C(scl_pin, sda_pin) # Connects the sda and scl to i2c
 
+
 Led = digitalio.DigitalInOut(board.GP0) # Set LED pin
 Led.direction = digitalio.Direction.OUTPUT # Pin is using output
 
 mpu = adafruit_mpu6050.MPU6050(i2c) # Sets up the MPU
 
-while True:
-    Xaccel = mpu.acceleration[0] #Reads the X acceleration
-    Yaccel = mpu.acceleration[1]
-    Zaccel = mpu.acceleration[2]
+with open("/data.csv", "a") as datalog:
+    while True:
+        time_elapsed = time.monotonic()
+        Xaccel = mpu.acceleration[0] #Reads the X acceleration
+        Yaccel = mpu.acceleration[1]
+        Zaccel = mpu.acceleration[2]
 
-    print(f"X acceleration:{Xaccel}") # Prints the X acceleration
-    print(f"Y acceleration:{Yaccel}")
-    print(f"Z acceleration:{Zaccel}")
-    print(" ") # Create a space between the prints
+        if abs(Zaccel) < 1: # If Z is 90 degrees from parallel
+            Led.value = True # Turn LED on
+            Tilted = True
+        else:
+            Led.value = False # Turn LED off
+            Tilted = False
+           
+        datalog.write(f"{time_elapsed},{Xaccel},{Yaccel},{Zaccel},{Tilted}\n")
 
-    if abs(Zaccel) < 1: # If Z is 90 degrees from parallel
-        Led.value = True # Turn LED on
-        time.sleep(.1) # Pause so there isn't too much data
-    else:
-        Led.value = False # Turn LED off
-        time.sleep(.1) # Pause so there isn't too much data
+        Led.value = True
+        time.sleep(.5)
+        Led.value = False
+
+        datalog.flush()
+        time.sleep(.1)
