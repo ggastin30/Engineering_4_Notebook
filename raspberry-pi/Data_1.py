5b41b5f612e5a3a5d8337867af1531f16f359dcd
ggastin30 <ggastin30@charlottesvilleschools.org>
2023-11-13 10:44:09 -0500

    Done w 1

--- raspberry-pi/Data_1.py
+++ raspberry-pi/Data_1.py
@@ -15,7 +15,7 @@ Led.direction = digitalio.Direction.OUTPUT # Pin is using output
 
 mpu = adafruit_mpu6050.MPU6050(i2c) # Sets up the MPU
 
-with open("/data.csv", "a") as datalog:
+with open("/data.csv", "a") as datalog: #When Data Mode is Active
     while True:
         time_elapsed = time.monotonic()
         Xaccel = mpu.acceleration[0] #Reads the X acceleration
@@ -24,16 +24,16 @@ with open("/data.csv", "a") as datalog:
 
         if abs(Zaccel) < 1: # If Z is 90 degrees from parallel
             Led.value = True # Turn LED on
-            Tilted = True
+            Tilted = True # Say if it's tilted
         else:
             Led.value = False # Turn LED off
-            Tilted = False
+            Tilted = False # Say if it's not tilted
            
-        datalog.write(f"{time_elapsed},{Xaccel},{Yaccel},{Zaccel},{Tilted}\n")
+        datalog.write(f"{time_elapsed},{Xaccel},{Yaccel},{Zaccel},{Tilted}\n") #Put the data into a chart
 
-        Led.value = True
+        Led.value = True # LED cycle
         time.sleep(.5)
         Led.value = False
 
-        datalog.flush()
+        datalog.flush() # Save the data
         time.sleep(.1)
